24c756de11c314dcd531b7da8662b9196ba23f65
Thien <dkfordark@gmail.com>
2021-10-21 00:09:30 +0700

    updated

---
+++ ltapPYthon/btapBuoi2pg8.py
@@ -0,0 +1,13 @@
+print("c30trg8")
+n,m = map(int,input("Vui long nhap 2 so nguyen duong cach nhau boi dau ,: ").split(","))
+i = 1
+c = 0
+print("cac uoc so chan cua %d: "%(n))
+while (i<m):
+    if (n%i ==0):
+        if (i%2 == 0):
+            c += 1
+            print("%d"%(i),end=" ")
+    i+=1
+if(c==0):
+    print("khong co uoc so chan nao nho hon %d"%(m))
